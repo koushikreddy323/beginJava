cbc3986860788f3e13b6d81c19688348ae5b9199
Koushik Reddy <koushikreddy323@gmail.com>
2021-03-11 12:18:27 +0530

    updated

--- Java/src/GuessTheNumber.java
+++ Java/src/GuessTheNumber.java
@@ -1,16 +1,35 @@
 import java.util.Scanner;
 import java.util.Random;
 class start{
-    public int getRandomNum(int max,int min){
+    public int setRandomNum() {
         Random rand = new Random();
-        return rand.nextInt(max-min)+min;
+        return rand.nextInt(10);
+    }
 
+
+
+    public  int getInfo(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your guessed number between 1 to 10");
+        return sc.nextInt();
     }
 }
+
 public class GuessTheNumber {
     public static void main(String[] args) {
         start s=new start();
-        System.out.println(s.getRandomNum(10,20));
+        int x=s.getInfo(); //user input
+        int y=s.setRandomNum(); //random system input
+        System.out.println(y);
+        if(x==y){
+            System.out.println("your guess is correct!");
+        }
+        else if(x<y){
+            System.out.println("your input is smaller");
+        }
+        else {
+            System.out.println("your input is bigger");
+        }
 
     }
 }
---
+++ Java/src/Inheritance.java
@@ -0,0 +1,45 @@
+//By inheritance we can all the functions from a parent class to child class. Here inh is parent class and inh2 is subclass.
+
+class inh{
+    private int x;
+    private int y;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+
+}
+class inh2 extends inh{
+    public int  pr(){
+        return getX();
+
+    }
+}
+
+class inh3 extends inh2{
+    inh3(){
+        System.out.println("Grand child");
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+    inh i=new inh3();
+    i.setX(10);
+    i.setY(20);
+        System.out.println(i.getX()+" "+ i.getY());
+
+
+    }
+}
