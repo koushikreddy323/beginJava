9a05640e2de0d6d23f4f8c79e59b9feda9bf1020
Koushik Reddy <koushikreddy323@gmail.com>
2021-06-14 12:56:16 +0530

    updated

---
+++ Java/src/swiftFood/Interfaces.java
@@ -0,0 +1,109 @@
+package swiftFood;
+/*
+Interface in java: It is a blueprint of a class which contains the
+abstract methods, static methods,default methods & public static final variable.
+Interface helps to achieve 100% abstraction
+any method inside an interface is by default considered as public abstract
+We cannot create the object of an interface directly.
+But indirectly we can create the object of an interface
+
+Rules of an interface:
+1. If a class has to attain the properties and behaviour of an interface then the class must
+    implement the interface using "implements" keyword
+2. In interface we can achieve multiple inheritance as one class can implement multiple interfaces
+3. We cannot directly create the object of an interface
+4. We can indirectly create the object of an interface using anonymous inner types
+5. We can directly create the reference of an interface
+6. We cannot implement one interface from another interface
+7. We can have extends relationship between 2 interfaces
+8. If a class partially implements an interface then it must declare itself as abstract
+9. Any method inside the interface is by default considered as public abstract
+10.We can have default method (specified with default keyword) and static method  (specified with default keyword)
+    inside an interface.
+11.Any variable inside an interface is considered as public static final and its value remains un-alte
+12.We can have an empty interface in java such interface is called as marker interface
+13.If a class extends another class and also implements an interface in such case the class must
+    ---> extend the other class first and
+    ---> then implement the other interface
+ */
+
+
+
+interface Animal1{
+    void eat();
+    void sleep();
+    void size();
+}
+class Tiger1 implements Animal1{
+    @Override
+    public void eat(){
+        System.out.println("Tiger eats meat");
+    }
+    @Override
+    public void sleep(){
+        System.out.println("Tiger is sleeping");
+    }
+
+    @Override
+    public void size() {
+        System.out.println("Size of Tiger");
+    }
+}
+
+class Deer1 implements Animal1 {
+    @Override
+    public void eat(){
+        System.out.println("Deer eats Trees and leaves");
+    }
+    @Override
+    public void sleep(){
+        System.out.println("Deer is sleeping");
+    }
+
+    @Override
+    public void size() {
+        System.out.println("Size of Deer");
+    }
+}
+
+class Monkey1 implements Animal1 {
+    @Override
+    public void eat(){
+        System.out.println("Monkey eats bananas");
+    }
+    @Override
+    public void sleep(){
+        System.out.println("Monkey sleeps");
+    }
+
+    @Override
+    public void size() {
+        System.out.println("Size of Monkey");
+    }
+}
+
+class Forest1 {
+    void permit( Animal1 a){
+        a.sleep();
+        a.eat();
+        a.size();
+        System.out.println("-------------");
+    }
+
+
+}
+public class Interfaces {
+    public static void main(String[] args) {
+        Tiger1 t=new Tiger1();
+        Deer1 d=new Deer1();
+        Monkey1 m=new Monkey1();
+        Forest1 f=new Forest1();
+        f.permit(t);
+        f.permit(d);
+        f.permit(m);
+
+    }
+}
+
+
+
---
+++ Java/src/swiftFood/inherit.java
@@ -0,0 +1,153 @@
+package swiftFood;
+
+
+/*
+class Player1 extends Object{
+    String name;//messi ---> ronaldo
+    String country;//Argentina ---> Portugal
+    int age;//33 ---> 35
+
+    public Player1(){
+        super();
+        System.out.println("Inside Player constructor");
+        name="messi";
+        country="Argentina";
+        age=33;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public int getAge() {
+        return age;
+    }
+
+}
+class FootBaller extends Player1{
+    FootBaller(String name,String country,int age){
+        super();
+        System.out.println("Inside Footballer constructor");
+        this.name=name;
+        this.country=country;
+        this.age=age;
+    }
+    void disp() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(country);
+    }
+}
+*/
+/*
+public class inherit {
+    public static void main(String[] args) {
+        FootBaller f1 = new FootBaller("Neymar", "Brazil", 30);
+//        System.out.println(f1.getName());//ronaldo
+//        System.out.println(f1.getCountry());//portugal
+//
+//        System.out.println(f1.getAge());//35
+        f1.disp();
+    }
+}
+
+
+
+
+
+class Player1 extends Object{
+    String name;// ronaldo
+    String country;// Portugal
+    int age;// 35
+
+    public Player1(String name,String country,int age){
+        super();
+        System.out.println("Inside Player constructor");
+        this.name=name;
+        this.country=country;
+        this.age=age;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public int getAge() {
+        return age;
+    }
+
+}
+class FootBaller extends Player1{
+    public FootBaller(String name,String country,int age){
+        super(name,country,age);
+        System.out.println("Inside Footballer parameterised constructor");
+
+    }
+    public FootBaller() {
+        this("messi","argentina",33);
+        System.out.println("Inside Footballer zero parameterised constructor");
+    }
+    void disp() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(country);
+    }
+}
+
+ */
+
+class inherit{
+    public static void main(String[] args) {
+cargoPlane cp=new cargoPlane();
+passengerPlane pp=new passengerPlane();
+cp.name="Messi";
+        System.out.println("Cargo plane details: ");
+        System.out.println(cp.name);
+        cp.fly();
+        cp.land();
+        cp.carryGoods();
+        cp.takeoff();
+        System.out.println("-------------------------");
+        System.out.println("passenger plane details");
+        pp.fly();
+        pp.land();
+        pp.carryPassengers();
+        pp.takeoff();
+    }
+}
+
+class plane{
+    String name;
+    void takeoff(){
+        System.out.println("plane is taking off");
+    }
+    void fly(){
+        System.out.println("plane flys at high heights");
+    }
+    void land(){
+        System.out.println("plane lands");
+    }
+}
+
+class cargoPlane extends plane{
+
+    @Override
+    void fly() {
+        System.out.println("Cargo plane flys at low height");;
+    }
+    void carryGoods(){
+        System.out.println("Cargo plane carry goods");
+    }
+}
+
+class passengerPlane extends plane{
+    @Override
+    void fly() {
+        System.out.println("Passenger plane flys at medium heights");
+    }
+    void carryPassengers(){
+        System.out.println("Passenger plane carrys passengers");
+    }
+}
