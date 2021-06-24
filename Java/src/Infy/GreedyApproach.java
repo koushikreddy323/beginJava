9bd816bdc31cda486749076257d72f9484e914bb
Koushik Reddy <koushikreddy323@gmail.com>
2021-06-24 12:24:02 +0530

    updated

--- Java/src/Infy/GreedyApproach.java
+++ Java/src/Infy/GreedyApproach.java
@@ -3,12 +3,11 @@ package Infy;
 //to the previous finish time. if the start time is greater than previous finish time then value of j will be assigned to i;
 public class GreedyApproach {
     //  1 3 0 5 8 5
-    public static int findMaxActivities(int start[], int finish[]) {              //  2 4 6 7 9 9
+    public static int findMaxActivities(int[] start, int[] finish) {              //  2 4 6 7 9 9
         int n=finish.length;
         int i = 0;  //4
-        int j=0;
         int count=1;               //1+1+1+1
-        for(j=1;j<n;j++){    //5
+        for(int j=1;j<n;j++){    //5
             if(start[j]>=finish[i]){
                 count++;
                 i=j;
@@ -18,8 +17,8 @@ public class GreedyApproach {
     }
 
     public static void main(String[] args) {
-        int start[] =  {1, 3, 0, 5, 8, 5};
-        int finish[] =  {2, 4, 6, 7, 9, 9};
+        int[] start =  {1, 3, 0, 5, 8, 5};
+        int[] finish =  {2, 4, 6, 7, 9, 9};
 
         System.out.println("Maximum number of activities: "+findMaxActivities(start, finish));
     }
